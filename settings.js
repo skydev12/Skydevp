0a08ae9b8041747126caab14038b223645b165b9
RISKY <107249536+skydev12@users.noreply.github.com>
2024-08-07 10:20:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188','6281356484612','6285773557984']
-global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.owner = ['6283829749607','6283829749607','6283829749607']
+global.packname = 'riskyx.my.id'
+global.author = 'risky.web.id'
+global.botname = 'sky bot'
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json'
 global.pairing_code = true
@@ -26,9 +26,9 @@ global.fake = {
 }
 
 global.my = {
-	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/nazedev',
-	gc: 'https://chat.whatsapp.com/Hx9vcBVhbc04KLVGPFtH2R',
+	yt: 'riskyx.my.id',
+	gh: 'risky.web.id',
+	gc: 'https://chat.whatsapp.com/Ew44LqoypCQGmnAVZan0Za',
 	ch: '120363250409960161@newsletter',
 }
 
